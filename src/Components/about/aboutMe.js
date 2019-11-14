b650e01e9d3874d3b43d4760b393b6e7d9c90202
patrickj188 <38990025+patrickj188@users.noreply.github.com>
2019-11-14 11:41:25 -0600

    About me
    Fixed about me page styles and made it mobile friendly

--- src/Components/about/aboutMe.js
+++ src/Components/about/aboutMe.js
@@ -1,90 +1,94 @@
-import React from 'react';
-import styled from 'styled-components';
-import me from '../../images/patrick.png'
+import React from "react";
+import styled from "styled-components";
+import me from "../../images/patrick.png";
 
+const AboutMe = () => {
+  // const AboutTitle = styled.h1`
+  //     font-family: 'Poppins', sans-serif;
+  //     font-size: 40px;
+  //     `
 
-const AboutMe = () =>{
+  // const DescriptionAboutMe = styled.div`
+  // width: 60%;
+  // font-family: 'Poppins', sans-serif;
+  // float: right;
+  // `;
 
-// const AboutTitle = styled.h1`
-//     font-family: 'Poppins', sans-serif;
-//     font-size: 40px;
-//     `
+  const PatrickPic = styled.img`
+    width: 280px;
+    height: 280px;
+    border-radius: 180px;
+    @media (max-width: 700px) {
+      width: 180px;
+      height: 180px;
+    }
+    @media (max-width: 400px) {
+      width: 120px;
+      height: 120px;
+    }
+  `;
 
-// const DescriptionAboutMe = styled.div`
-// width: 60%;
-// font-family: 'Poppins', sans-serif;
-// float: right;
-// `;
+  const AboutContainer = styled.div`
+    width: 60%;
+    margin: 0 auto;
+    height: 50vh;
+    display: flex;
+    flex-direction: row;
+    @media (max-width: 1000px) {
+      width: 100%;
+    }
+  `;
 
-const PatrickPic = styled.img`
-width: 280px;
-height: 280px;
-border-radius: 180px;
-`;
+  const PatrickPicContainer = styled.div`
+    width: 40vw;
+    height: 40vh;
+    position: relative;
+    top: 5vh;
+    margin: 0 2%;
+  `;
 
+  const DescriptionContainer = styled.div`
+    width: 80vw;
+    height: 40vh;
+    position: relative;
+    top: 5vh;
+    margin: 0 1%;
+  `;
 
-const AboutContainer = styled.div`
-width: 100%;
-height: 50vh;
-border: 1px solid grey;
-display: flex;
-  flex-direction: row;
-`;
+  const AboutText = styled.p`
+    padding-top: 10%;
+    color: #393f4d;
+    font-size: 18px;
+    font-family: "Poppins", sans-serif;
+    @media (max-width: 700px) {
+      padding-top: 1%;
+    }
+  `;
 
-const PatrickPicContainer = styled.div`
-width: 40vw;
-height: 40vh;
-border: 1px solid black; 
-position: relative;
-top: 5vh;
-margin: 0 2%; 
-`;
+  return (
+    <div>
+      <AboutContainer>
+        <PatrickPicContainer>
+          <PatrickPic src={me} />
+        </PatrickPicContainer>
+        <DescriptionContainer>
+          <AboutText>
+            Hello, my name is Patrick! I am an innovative, self-starter, and
+            creative full stack developer from Dallas, TX. Welcome to my
+            portfolio! Take a look around, and if you like what you see, give me
+            a shout and let's talk!
+          </AboutText>
+        </DescriptionContainer>
+      </AboutContainer>
 
-const DescriptionContainer = styled.div`
-width: 80vw;
-height: 40vh;
-border: 1px solid black; 
-position: relative;
-top: 5vh;
-margin: 0 2%;
-&:p {
-    padding: 50px 0;
-}
-
-`;
-
-    return (
-        <div>
-
-        <AboutContainer>
-            <PatrickPicContainer>
-                <PatrickPic src={me} />
-            </PatrickPicContainer>
-            <DescriptionContainer>
-                <p>My name is Patrick; I am a full-stack web developer. 
-Are you looking for an innovative, self-starter, and creative team member? Give me a shout and let's talk!</p>
-
-            </DescriptionContainer>
-
-        </AboutContainer>
-
-
-
-
-
-
-
-
-
-            {/* <AboutTitle>About Me</AboutTitle>
+      {/* <AboutTitle>About Me</AboutTitle>
             <PatrickPic src={me} />
             <DescriptionAboutMe>
             I am a full stack developer from Dallas, TX. My adventure in coding began last year and I've been having fun ever since. 
             Are you looking for an innovative, self-starter, and creative team member? Give me a shout and let's talk!
             </DescriptionAboutMe> */}
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default AboutMe;
