58a9f2ceb708018924faa62fed69b9791a852de4
patrickj188 <38990025+patrickj188@users.noreply.github.com>
2020-05-08 11:56:29 -0500

    Fixed sizing
    Fixed the sizing on the project divs

--- src/Components/projects/ProjectContainer.js
+++ src/Components/projects/ProjectContainer.js
@@ -1,54 +1,56 @@
-import React from 'react';
-import projectItems from '../../projectList.json'
-import ProjectList from './ProjectList';
-import styled from 'styled-components';
-import ProjectLarge from './ProjectLarge';
+import React from "react";
+import projectItems from "../../projectList.json";
+import ProjectList from "./ProjectList";
+import styled from "styled-components";
+import ProjectLarge from "./ProjectLarge";
 import "./projectStyle.css";
-import { CSSTransition } from 'react-transition-group';
-
-
-
-
+import { CSSTransition } from "react-transition-group";
 
 const ProjectContainer = () => {
-
-    const ContainerProject = styled.div`
+  const ContainerProject = styled.div`
     display: block;
     justify-content: center;
     margin: 0auto;
-    -webkit-animation: fadein 2s; 
-       -moz-animation: fadein 2s; 
-        -ms-animation: fadein 2s; 
-         -o-animation: fadein 2s; 
-            animation: fadein 2s;
-    `;
-    const ImgDiv = styled.div`
+    -webkit-animation: fadein 2s;
+    -moz-animation: fadein 2s;
+    -ms-animation: fadein 2s;
+    -o-animation: fadein 2s;
+    animation: fadein 2s;
+  `;
+  const ImgDiv = styled.div`
     display: flex;
     flex-wrap: wrap;
-   justify-content: center;
-   &:hover {
-    opacity: 100;
-  }
-  @media (max-width: 950) {
-    flex-direction: column;
-  }
-`;
+    justify-content: center;
+    &:hover {
+      opacity: 100;
+    }
+    @media (max-width: 950) {
+      flex-direction: column;
+    }
+  `;
 
-    let porjectLarge = projectItems.map(item => {
-        return (<ProjectLarge className="imgDiv" className={item.id % 2 ? 'odd' : "even"} key={item.id} name={item.project}
-            about={item.about} website={item.website} gitHub={item.githubLink} img={item.img} />)
-    })
-
-
+  let projectLarge = projectItems.map(item => {
     return (
-        <div className="fadein">
-            <ContainerProject >
-                <ImgDiv>
-                    {porjectLarge}
-                </ImgDiv>
-            </ContainerProject>
-        </div>
-    )
+      <ProjectLarge
+        className="imgDiv"
+        className={item.id % 2 ? "odd" : "even"}
+        key={item.id}
+        name={item.project}
+        about={item.about}
+        website={item.website}
+        gitHub={item.githubLink}
+        img={item.img}
+      />
+    );
+  });
+
+  return (
+    <div className="fadein">
+      <ContainerProject>
+        <ImgDiv>{projectLarge}</ImgDiv>
+      </ContainerProject>
+    </div>
+  );
 };
 
 export default ProjectContainer;
